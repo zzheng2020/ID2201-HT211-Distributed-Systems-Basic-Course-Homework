723d0510467dd724be11aedc2a9f4d4b1b2e8115
Ziheng Zhang <mrzhangziheng@gmail.com>
2021-09-18 02:27:00 +0200

    finished iterate function of dijkstra.erl

--- Routy-a small  routing network/dijkstra.erl
+++ Routy-a small  routing network/dijkstra.erl
@@ -10,14 +10,14 @@
 -author("Ziheng Zhang").
 
 %% API
--export([update/4]).
+-export([update/4, iterate/3]).
 
 %% returns the length of the shortest path to the
 %% node or 0 if the node is not found.
 entry(Node, Sorted) ->
     case lists:keyfind(Node, 1, Sorted) of
-        {_, Length_of_the_shortest_path, _} ->
-            Length_of_the_shortest_path;
+        {_, Length_of_hops, _} ->
+            Length_of_hops;
         false ->
             0
     end.
@@ -36,10 +36,40 @@ replace(Node, N, Gateway, Sorted) ->
 %% Only if we have a better (shorter) path should we
 %% replace the existing entry.
 update(Node, N, Gateway, Sorted) ->
-    Length_of_the_shortest_path = entry(Node, Sorted),
+    Length_of_hops = entry(Node, Sorted),
     if
-        N >= Length_of_the_shortest_path ->
+        N >= Length_of_hops ->
             Sorted;
         true ->
             replace(Node, N, Gateway, Sorted)
+    end.
+
+%% construct a table given a sorted list of nodes,
+%% a map and a table constructed so far.
+iterate(Sorted, Map, Table) ->
+    io:format("Sorted: ~w~n", [Sorted]),
+    io:format("Map: ~w~n", [Map]),
+    io:format("Table: ~w~n", [Table]),
+    case Sorted of
+        [] ->
+            Table;
+        [{_, inf, _} | _] ->
+            Table;
+        [Entry | Tail] ->
+            io:format("Entry: ~w~n", [Entry]),
+            io:format("Tail: ~w~n", [Sorted]),
+            {Node, Length, Gateway} = Entry,
+
+            case lists:keyfind(Node, 1, Map) of
+                {_, ToCity} ->
+                    io:format("~w~n", [ToCity]),
+                    NewSorted = lists:foldl(fun(City, TmpSorted) -> update(City, Length + 1, Gateway, TmpSorted) end,
+                                              Tail, ToCity);
+                false ->
+                    NewSorted = Tail
+            end,
+
+            io:format("NewList: ~w~n", [NewSorted]),
+            io:format("===========================~n"),
+            iterate(NewSorted, Map, [{Node, Gateway} | Table])
     end.
