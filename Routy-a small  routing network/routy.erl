9e4dc32e5e9b98dbc8c32f000ca6ece486bfebfe
Ziheng Zhang <mrzhangziheng@gmail.com>
2021-09-21 15:34:19 +0200

    finished routy.erl

--- Routy-a small  routing network/routy.erl
+++ Routy-a small  routing network/routy.erl
@@ -28,47 +28,13 @@ init(Name) ->
     Hist = hist:new(Name),
     router(Name, 0, Hist, Intf, Table, Map).
 
-% a symbolic name such as london
-% a counter
-% a history of received messages
-% a set of interfaces
-% a routing table
-% a map of the network
 router(Name, N, Hist, Intf, Table, Map) ->
     receive
-    % if the name is matched, then no further hops needed
-        {route, Name, From, Message} ->
-            io:format("~w: received message (~s) from ~w~n", [Name, Message, From]),
-            router(Name, N, Hist, Intf, Table, Map);
 
-    % if the name is not matched, then forward to another hop
-        {route, To, From, Message} ->
-            io:format("~w: routing message (~s) from ~w to ~w~n", [Name, Message, From, To]),
-            case dijkstra:route(To, Table) of
-                {ok, Gw} ->
-                    case interfaces:lookup(Gw, Intf) of
-                        notfound ->
-                            io:format("~w: interface for gw ~w not found ~n", [Name, Gw]);
-                        {ok, Pid} ->
-                            io:format("~w: forward to ~w~n", [Name, Gw]),
-                            Pid ! {route, To, From, Message}
-                    end;
-                notfound ->
-                    io:format("~w: routing entry for ~w not found ~n", [Name, To])
-            end,
-            % The router process will continuously listen
-            router(Name, N, Hist, Intf, Table, Map);
-
-    % add a message so that a local user can initiate the routing of a
-    % message without knowing the name of the local router.
         {send, To, Message} ->
             self() ! {route, To, Name, Message},
             router(Name, N, Hist, Intf, Table, Map);
 
-    % r1 ! {add, goteborg, {r3, 'sweden@192.168.1.3'}
-    % stockholm(r1) => goteborg(r3)
-    % a monitor will send a ’DOWN’ message to the process
-    % and we can then remove links to the node
         {add, Node, Pid} ->
             Ref = erlang:monitor(process, Pid),
             Intf1 = interfaces:add(Node, Ref, Pid, Intf),
@@ -92,8 +58,6 @@ router(Name, N, Hist, Intf, Table, Map) ->
             Intf1 = interfaces:remove(Down, Intf),
             router(Name, N, Hist, Intf1, Table, Map);
 
-    % When receiving a links-state message a
-    % router must check if this is an old or new message
         {links, Node, X,Links} ->
             case hist:update(Node, X, Hist) of
                 {new, Hist1} ->
@@ -104,11 +68,8 @@ router(Name, N, Hist, Intf, Table, Map) ->
                     router(Name, N, Hist, Intf, Table, Map)
             end;
 
-    % The status, From message can be used to do a pretty-print of the
-    % state. Add a function that sends a status message to a process, receives
-    % the reply and displays the information.
         {status, From} ->
-            io:format("STATUS info:"),
+            io:format("STATUS messages:"),
             From ! {status, {Name, N, Hist, Intf, Table, Map}},
             router(Name, N, Hist, Intf, Table, Map);
 
@@ -116,12 +77,32 @@ router(Name, N, Hist, Intf, Table, Map) ->
             Table1 = dijkstra:table(interfaces:list(Intf), Map),
             router(Name, N, Hist, Intf, Table1, Map);
 
-    % order our router to broadcast a link-state message
         broadcast ->
             Message = {links, Name, N, interfaces:list(Intf)},
             interfaces:broadcast(Message, Intf),
-            % N+1 records the number of times of broadcast-ordering
             router(Name, N+1, Hist, Intf, Table, Map);
+
+    % name is matched, message arrived.
+        {route, Name, From, Message} ->
+            io:format("~w: received message (~s) from ~w~n", [Name, Message, From]),
+            router(Name, N, Hist, Intf, Table, Map);
+
+    % name is not matched, need to find next hop.
+        {route, To, From, Message} ->
+            io:format("~w: routing message (~s) from ~w to ~w~n", [Name, Message, From, To]),
+            case dijkstra:route(To, Table) of
+                {ok, Gw} ->
+                    case interfaces:lookup(Gw, Intf) of
+                        notfound ->
+                            io:format("~w: interface for gw ~w not found ~n", [Name, Gw]);
+                        {ok, Pid} ->
+                            io:format("~w: forward to ~w~n", [Name, Gw]),
+                            Pid ! {route, To, From, Message}
+                    end;
+                notfound ->
+                    io:format("~w: routing entry for ~w not found ~n", [Name, To])
+            end,
+            router(Name, N, Hist, Intf, Table, Map);
 
         stop ->
             ok
